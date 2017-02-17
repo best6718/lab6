2c8247bd317ffc65a628b110fc7aa5272ad14cf8
Karena Huang <khuang@mills.edu>
2017-02-17 19:47:05 +0000

    additional changes

--- app_server/controllers/labsix.js
+++ app_server/controllers/labsix.js
@@ -1,3 +1,8 @@
 module.exports.index = function(req, res){
   res.render('html1', { title: 'pageone' });
 };
+
+module.exports.index2 = function(req, res){
+  res.render('html2', { title: 'pagetwo' });
+};
+
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlMain = require('../controllers/locations');
 var labsix = require('../controllers/labsix');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
 router.get('/pageone', labsix.index);
-
+router.get('/pagetwo', labsix.index2);
 module.exports = router;
